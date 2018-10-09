1a507fc0afa118d9f91d1139f6caaba763003bdc
mattperkins <github@mattperkins.com>
2018-10-09 21:38:53 +0100

    refactor promise

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,13 +4,18 @@ var router = express.Router();
 const knex = require('../db/knex') // connection to db/config
 /* GET users. */
 router.get('/', (req, res, next) => {
-  // res.render('users', { title: 'Users' });
+// res.render('users', { title: 'Users' });
   knex('users') // connect to db tbl
   .select() // select all rows in tbs
-  //then render all rows to view
-  .then(users => {
-    res.render('users', {users: users}) // 'users' = users.hbs // {users: users} // property and array of data from users tbl 
-})
+//then render all rows to view
+//  .then(res => res.json)
+  // .then(users => { 
+   
+.then(peach => res.send({ lime : peach }))
+ 
+    // res.render('users', {users: users}) 
+// 'users' = users.hbs // {users: users} // property and array of data from users tbl
+
 });
 
 module.exports = router;
