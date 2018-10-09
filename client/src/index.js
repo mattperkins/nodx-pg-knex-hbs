eecd7fa79145b7e0080e723e629778a0c6e1483c
mattperkins <github@mattperkins.com>
2018-10-09 21:47:52 +0100

    add client

---
+++ client/src/index.js
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+// Modest Grid linked via public/index.html
+import './css/reset.css'
+import './css/style.css'
+// main ("ROOT") component 
+class Root extends React.Component {
+  state = { lemons: [] }
+
+  componentDidMount() {
+      fetch('/users')
+      .then(res => res.json())  
+      .then(plum => {
+        console.log(plum.lime)
+        this.setState({ lemons: plum.lime })
+      })
+  }
+
+
+render() {
+ return (
+
+
+<div className="wrapper">
+<div className="row">
+
+<div className="col-12">
+
+{this.state.lemons.map(x => <p key={x.id}>{x.description}</p>)}
+
+</div>
+
+</div>
+</div>
+
+
+)// end return
+}// end render
+}// end component
+
+ReactDOM.render(<Root />, 
+  document.getElementById('root'))
+  registerServiceWorker()
