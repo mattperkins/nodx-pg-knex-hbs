b3028d2e0d67322ada10de85f1c8f67c57d83993
mattperkins <github@mattperkins.com>
2018-11-01 20:32:39 +0000

    format

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,21 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const knex = require('../db/knex') // connection to db/config
+const knex = require("../db/knex"); // connection to db/config
 /* GET users. */
-router.get('/', (req, res, next) => {
-// res.render('users', { title: 'Users' });
-  knex('users') // connect to db tbl
-  .select() // select all rows in tbs
-//then render all rows to view
-//  .then(res => res.json)
-  // .then(users => { 
-   
-.then(peach => res.send({ lime : peach }))
- 
-    // res.render('users', {users: users}) 
-// 'users' = users.hbs // {users: users} // property and array of data from users tbl
+router.get("/", (req, res, next) => {
+  // res.render('users', { title: 'Users' });
+  knex("users") // connect to db tbl
+    .select() // select all rows in tbs
+    //then render all rows to view
+    //  .then(res => res.json)
+    // .then(users => {
 
+    .then(peach => res.send({ lime: peach }));
+
+  // res.render('users', {users: users})
+  // 'users' = users.hbs // {users: users} // property and array of data from users tbl
 });
 
 module.exports = router;
